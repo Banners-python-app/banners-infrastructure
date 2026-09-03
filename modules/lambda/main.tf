b6acf319a0445d2a24c23515bdbd0ec0d89038a9
abhishek-bakare <abhishekbakare43398@gmail.com>
2026-09-04 00:19:12 +0530

    added checkov comments

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -2,7 +2,8 @@
 # cloudwatch logs storage costs
 
 resource "aws_cloudwatch_log_group" "this" {
-    # checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year"
+    # checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year"2
+    # checkov:skip= CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS"
     name = "/aws/lambda/${var.function_name}"
     retention_in_days = var.retention_in_days
     tags = {
